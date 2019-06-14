ca65e40bc67df65782f019985a5d7102f44c54c9
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-14 05:56:16 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -30,3 +30,24 @@
 # end
 
 # practice
+
+def first_challenge
+  contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_icecream_flavors: ["chocolate", "vanilla"],
+    knows: nil
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
+
+
+
+
+
+end
