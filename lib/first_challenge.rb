5353e25cb1b995de10fee6cd5f016c398fcd53f3
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-14 06:14:21 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -46,8 +46,14 @@ def first_challenge
   }
 }
 
+contacts.each do |person,data|
+  data.each do |key,value|
+    if key == :favorite_icecream_flavors
+      delete_if { |ice_cream| ice_cream == "strawberry"  }
 
 
 
 
+
+end
 end
