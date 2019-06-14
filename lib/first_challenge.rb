868e4cb95f411d433de686111c1da25adcd319bc
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-14 05:53:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,30 +1,38 @@
-def first_challenge
-  contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we",
-      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
-      knows: nil
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
-  }
+#def first_challenge
+#  contacts = {
+#    "Jon Snow" => {
+#      name: "Jon",
+#      email: "jon_snow@thewall.we",
+#      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
+#      knows: nil
+#    },
+#    "Freddy Mercury" => {
+#      name: "Freddy",
+#      email: "freddy@mercury.com",
+#      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+#    }
+#  }
 
   #your code here
 
-  contacts.each do |person,data|
-    data.each do |attribute,value|
-      if attribute == :favorite_icecream_flavors
-        value.delete_if { |ice_cream| ice_cream == "strawberry"}
+#  contacts.each do |person,data|
+#    data.each do |attribute,value|
+#      if attribute == :favorite_icecream_flavors
+#        value.delete_if { |ice_cream| ice_cream == "strawberry"}
 
 
   #remember to return your newly altered contacts hash!
 
-end
-end
-end
-return contacts
-end
+#end
+#end
+#end
+#return contacts
+# end
+
+# practice
+
+
+
+
+
+
